a9a0c8eac48afdbd2f68693d94ffcab61abbd2c9
Chigoziem Nwaiwu <princegoziem@gmail.com>
2020-05-03 22:26:15 +0100

    Updates

---
+++ precache-manifest.e754d1c336e7c5938ce7b43244a63dfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd9adc960b734602bb10c3e99478e20",
+    "url": "/the-plan-board/index.html"
+  },
+  {
+    "revision": "806d3dbcd2d84629be49",
+    "url": "/the-plan-board/static/css/2.412fd7e3.chunk.css"
+  },
+  {
+    "revision": "79b6a4607c6d924789bd",
+    "url": "/the-plan-board/static/css/main.34a03a39.chunk.css"
+  },
+  {
+    "revision": "806d3dbcd2d84629be49",
+    "url": "/the-plan-board/static/js/2.ecf8aae8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/the-plan-board/static/js/2.ecf8aae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b6a4607c6d924789bd",
+    "url": "/the-plan-board/static/js/main.475b61c1.chunk.js"
+  },
+  {
+    "revision": "c993214796e4198357d9",
+    "url": "/the-plan-board/static/js/runtime-main.0f767992.js"
+  }
+]);
